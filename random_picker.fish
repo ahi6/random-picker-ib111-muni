5fa45e237c03d6717f7a76327a756c4740ddfc71
ahi6 <60984726+ahi6@users.noreply.github.com>
2026-01-01 17:33:43 +0100

    Move from Gist to repo

---
+++ random_picker.fish
@@ -0,0 +1,27 @@
+#!/run/current-system/sw/bin/fish
+
+# vscode is the preferred editor for ib111 exams
+if which code >/dev/null 2>/dev/null
+  set editor code
+else if which codium >/dev/null 2>/dev/null
+  set editor codium
+else
+  set editor $EDITOR
+end
+
+set path $(find . -regextype egrep -regex "\.\/(([0-1][1-9])|10)\/(p|v|r)[0-6]_.*" | shuf -n 1)
+echo 🐱 Picking $path
+if not timeout 5 python $path >/dev/null 2>/dev/null
+  if cat $path | grep "†" >/dev/null
+    echo 🐈️ Too difficult. Rerunning.
+    exec fish (status filename)
+  else
+    echo 😺 Chosen $path
+    exec $editor $path
+    cd $(dirname $path)
+  end
+else
+  echo 😼 Already done. Rerunning.
+  exec fish (status filename)
+end
+
